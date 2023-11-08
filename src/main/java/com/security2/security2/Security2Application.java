92389dd0b280df0aa664becb2c4214266e1e53e4
PrabhatSuyal <132249743+PrabhatSuyal@users.noreply.github.com>
2023-11-08 11:27:51 +0530

    Update Security2Application.java

--- src/main/java/com/security2/security2/Security2Application.java
+++ src/main/java/com/security2/security2/Security2Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Security2Application {
 
+	// blog : https://www.bezkoder.com/spring-boot-jwt-authentication/
+	// github : https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication/blob/master/src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+	// local project : C:\Program Files\JetBrains\IntelliJ_workspace\security2
 	public static void main(String[] args) {
 		SpringApplication.run(Security2Application.class, args);
 	}
